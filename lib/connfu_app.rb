7235478ce2bcb5b0d35c257a91ab51e33b00ea1c
Kalvir Sandhu <kalv@kalv.co.uk>
2011-07-18 17:43:55 +0100

    For now simply show the one connfu url and password when being redirected from heroku addon

--- lib/connfu_app.rb
+++ lib/connfu_app.rb
@@ -26,4 +26,9 @@ class ConnfuApp < Sinatra::Base
     result = { :id => "1", :config => { "CONNFU_URL" => "jid://usera@127.0.0.1", "CONNFU_PASSWD" => "1" } }
     result.to_json
   end
+
+  get "/heroku/resources/:id" do
+    # authenticate
+    "Your details are CONNFU_URL=jid://usera@127.0.0.1 and CONNFU_PASSWD=1"
+  end
 end
