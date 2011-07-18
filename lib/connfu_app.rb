414098f981b32200a6d49c8d5ce9e7c153773f3b
Kalvir Sandhu <kalv@kalv.co.uk>
2011-07-18 16:37:50 +0100

    Heroku requires provisioning requests to use HTTP basic auth using the id and password defined in the manifest file

--- lib/connfu_app.rb
+++ lib/connfu_app.rb
@@ -6,7 +6,22 @@ class ConnfuApp < Sinatra::Base
     "Connfu Heroku Addon app"
   end
 
+  helpers do
+    def protected!
+      unless authorized?
+        response['WWW-Authenticate'] = %(Basic realm="Connfu Heroku Addon HTTP Auth")
+        throw(:halt, [401, "Not authorized\n"])
+      end
+    end
+
+    def authorized?
+      @auth ||=  Rack::Auth::Basic::Request.new(request.env)
+      @auth.provided? && @auth.basic? && @auth.credentials && @auth.credentials == ['connfu', 'zPtRdLgyWBsnDWaL']
+    end
+  end
+
   post "/heroku/resources" do
+    protected!
     # Create the connfu user and relevant details
     result = { :id => "1", :config => { "CONNFU_URL" => "usera@127.0.0.1", "CONNFU_PASSWD" => "1" } }
     result.to_json
